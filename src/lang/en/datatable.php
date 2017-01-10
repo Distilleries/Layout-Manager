310f9c121073b4af88b8e3993e04c9705ad9c8d3
Maxime François <mfrancois@big-boss-studio.com>
2017-01-10 12:02:57 +0100

    Add datatable translation

---
+++ src/lang/en/datatable.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'id'          => 'ID',
+    'libelle'     => 'Title',
+    'css_class'   => 'Backend CSS class',
+    'html'        => 'HTML',
+    'plugins'     => 'TinyMCE Plugins',
+    'toolbar'     => 'TinyMCE Toolbar',
+    'templatable' => 'Contenu dynamique',
+];
