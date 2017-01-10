5214405c16e3c1fbd2d9017c678aedacee569ee5
Maxime François <mfrancois@big-boss-studio.com>
2017-01-10 10:31:06 +0100

    Remove api from web

--- src/Distilleries/routes/web.php
+++ src/Distilleries/routes/web.php
@@ -1,11 +1,5 @@
 <?php
 
-$router->group(['middleware' => ['api']], function () use($router) {
-
-    $router->controller('template', 'Api\TemplateController');
-
-});
-
 $router->group(array('middleware' => 'auth'), function() use($router)
 {
     $router->group(array('middleware' => 'permission', 'prefix' => config('expendable.admin_base_uri')), function() use($router)
