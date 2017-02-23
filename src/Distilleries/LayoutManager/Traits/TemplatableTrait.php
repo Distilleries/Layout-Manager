cd5ba40694f65542f127551ca51b1eddd7dcab6f
Jean-François Meinesz <jfmeinesz@gmail.com>
2017-02-23 16:59:20 +0100

    Get template class

--- src/Distilleries/LayoutManager/Traits/TemplatableTrait.php
+++ src/Distilleries/LayoutManager/Traits/TemplatableTrait.php
@@ -8,4 +8,34 @@ trait TemplatableTrait
     {
         return $this->morphToMany('Distilleries\LayoutManager\Template', 'templatable')->withPivot('html', 'category', 'libelle', 'order', 'status', 'id')->orderBy('order');
     }
+
+    public function afterRenderingTemplate($html) {
+        return $html;
+    }
+
+    public function getTemplateByClass($css_class, $index = 0, $noHeading = false) {
+        $tpl = $this->templates->where('css_class', $css_class)->pluck('pivot.html')->toArray()[$index];
+        if ($noHeading) {
+            $tpl = preg_replace('/h[1-6]/', 'div', $tpl);
+        }
+        return $this->afterRenderingTemplate($tpl);
+    }
+
+
+    public function getTemplateByCategory($category, $index = 0, $noHeading = false) {
+        $tpl = $this->templates->where('pivot.category', $category)->pluck('pivot.html')->toArray()[$index];
+        if ($noHeading) {
+            $tpl = preg_replace('/h[1-6]/', 'div', $tpl);
+        }
+        return $this->afterRenderingTemplate($tpl);
+    }
+
+
+    public function getTemplateByCategoryAndClass($category, $css_class, $index = 0, $noHeading = false) {
+        $tpl = $this->templates->where('css_class', $css_class)->where('pivot.category', $category)->pluck('pivot.html')->toArray()[$index];
+        if ($noHeading) {
+            $tpl = preg_replace('/h[1-6]/', 'div', $tpl);
+        }
+        return $this->afterRenderingTemplate($tpl);
+    }
 }
